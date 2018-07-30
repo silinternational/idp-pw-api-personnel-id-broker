c88fb189796d93f30fcc80b63a2e21b6d28c3f21
Matt Henderson <matt_henderson@sil.org>
2018-07-30 11:25:31 -0400

    Update run-broker script (for tests) to use Logentries key (if present)

--- run-broker.sh
+++ run-broker.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+if [[ "x" == "x$LOGENTRIES_KEY" ]]; then
+    echo "Missing LOGENTRIES_KEY environment variable";
+else
+    echo "Found LOGENTRIES_KEY environment variable";
+
+    # Set logentries key based on environment variable
+    sed -i /etc/rsyslog.conf -e "s/LOGENTRIESKEY/${LOGENTRIES_KEY}/"
+    # Start syslog
+    rsyslogd
+
+    # Give syslog time to fully start up.
+    sleep 10
+fi
+
 # Try to install composer dev dependencies
 cd /data
 composer install --no-interaction --no-scripts
